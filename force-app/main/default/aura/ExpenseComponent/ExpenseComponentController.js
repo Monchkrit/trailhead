0d0aff6183642d55cb96695c53221c2898cf1a6f
monchkrit <monchkrit@gmail.com>
2021-03-13 00:32:20 -0800

    moving to laptop

--- force-app/main/default/aura/ExpenseComponent/ExpenseComponentController.js
+++ force-app/main/default/aura/ExpenseComponent/ExpenseComponentController.js
@@ -1,5 +1,8 @@
 ({
-    myAction : function(component, event, helper) {
-
+    clickReimbursed : function(component, event, helper) {
+        let expense = component.get("v.expense");
+        let updateEvent = component.getEvent("updateExpense");
+        updateEvent.setParams({ "expense": expense });
+        updateEvent.fire();
     }
 })
